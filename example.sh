f89ac51edebc62e35c9a41ccc72a7078d0b18b07
Jacques de Vos <jacques.devos@fitillion.com>
2025-02-21 10:55:40 +1100

    script to run end to end example

---
+++ example.sh
@@ -0,0 +1,4 @@
+unzip exampledocuments.zip -d exampledocs
+./create-index.sh -documents exampledocs
+# ./search.sh -prompt "What is the capital of France?" -limit 10
+# ./prompt.sh -query "What is the capital of France?" -documentPaths "exampledocs/france.txt"
