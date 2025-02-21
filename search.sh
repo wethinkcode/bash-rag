85d5c2900529a9bafa607a0540de61e7e445535d
Jacques de Vos <jacques.devos@fitillion.com>
2025-02-21 12:15:45 +1100

    remove punctuation, which is not compatible with fts

--- search.sh
+++ search.sh
@@ -2,7 +2,7 @@ if [ "$#" -ne 4 ] || [ "$1" != "-prompt" ] || [ "$3" != "-limit" ]; then
     echo "Usage: $0 -prompt <search_prompt> -limit <max_results>"
     exit 1
 fi
-search_prompt=$(echo "$2")
+search_prompt=$(echo "$2" | tr -d '[:punct:]')
 max_amount_of_docs_to_return="$4"
 rowids_list=$(sqlite3 rag-textsearch.db "SELECT rowid FROM documents WHERE documents MATCH '$search_prompt' ORDER BY rank LIMIT $max_amount_of_docs_to_return;")
 rowids_count=$(echo "$rowids_list" | wc -l)
